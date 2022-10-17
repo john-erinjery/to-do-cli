482637b9dea8f1b9c9a66883aee5688ca46c592e
John Erinjery <jancyvinod415@gmail.com>
2022-10-17 23:49:02 +0530

    finish 'create' help doc

--- docs.py
+++ docs.py
@@ -51,7 +51,7 @@ Options:
                             set to the current date by default.
     -t, --time              Time for the Task completion. {3}
     -det, --details         Further details on the task.
-
+    -o, --output
 {4}
             '''.format(colored('todo create \'Do the Dishes\'', 'green'), colored('Usage : todo create <task> [<options>]', attrs=['bold']),\
                 colored('(eg : -d 23 Oct 2020)', attrs=['dark']), colored('(eg : -t 2.00 pm)', attrs=['dark']),\
@@ -69,32 +69,36 @@ Options:
 # backend-functions
 # These functions are not part of the main program, they are present to support the main functions.
 
-def _check_config_file() -> bool:
-    """
-    Configuration directory check
-
-    This functions checks the .config directory at 'C:/Users/USERNAME/.config' for 'todo.json' file.
-    If the file is not present, it will return False, else True.
-    """
-    config_dir_path = 'C:/Users/{}/.config'.format(os.getenv('USERNAME'))
-    global config_file_path
-
-    config_file_path = config_dir_path + '/todo.json'
-    if os.path.exists(config_file_path):
-        return True
-    else:
-        return False
-
 def _config_dictionary() -> dict:
     '''
-    Returns the configuration dictionary for todo
-    '''
+    Returns the configuration dictionary for todo.
 
-    if _check_config_file():
+    This functions checks the .config directory at 'C:/Users/USERNAME/.config' for 'todo.json' file.
+    If the file is not present, it will return the default configurations.
+    If the file is present it will return the updated version of the default config file
+    '''
+    config_dir_path = 'C:/Users/{}/.config'.format(os.getenv('USERNAME'))
+    config_file_path = config_dir_path + '/todo.json'
+    if os.path.exists(config_file_path):
         with open(config_file_path) as f:
-            return load(f)
+            user_config = load(f)
+            default_config = load(open('config.json'))
+            default_config.update(user_config)
+            return default_config
     else:
         return load(open('config.json'))
         
 def create(id, name:str, time:str="", det:str='',date:str=datetime.date.today().strftime("%d-%m-%Y")):
-    pass
+    '''
+    The create function of todo cli
+
+    The function takes in a total of 5 arguments of which three are optional.
+    1. id    - defines an id uniqueto the task. (required)
+    2. name  - a name given to the task, word count prefereably be minimum. (required)
+    3. time  - the time for task completion in the format chosen by the user. (optional: default value is nil)
+    4. det   - details for the task. (optional: default value is nil)
+    5. date  - the date for task completion. (optional: default value is the day of task assignment)
+    '''
+    config = _config_dictionary()
+
+
